cbf389688eebcdbf6fdeb10845e08aef216963c0
Mubashir Nisar <mubashirnhasir@gmail.com>
2025-10-21 01:13:23 +0530

    fix: correct font imports and update metadata description in layout component

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,20 @@
-import { GeistSans, GeistMono } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react"; // safer than /next for app router
+import { Analytics } from "@vercel/analytics/next"
 
-const geistSans = GeistSans({
+const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
-const geistMono = GeistMono({
+const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
 export const metadata = {
   title: "Ninja Job Support",
-  description: "Get real-time job and proxy interview support from experts.",
+  description: "Ninja job support",
 };
 
 export default function RootLayout({ children }) {
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <Analytics />
+        <Analytics/>
       </body>
     </html>
   );
